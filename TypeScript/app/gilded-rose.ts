ea4db579bb2038f4b3835fa5b17f56ba2c83977e
karaaliidris <karaaliidris@gmail.com>
2022-07-26 14:30:00 +0300

    first commit

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,6 +17,7 @@ export class GildedRose {
     this.items = items;
   }
 
+  
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
