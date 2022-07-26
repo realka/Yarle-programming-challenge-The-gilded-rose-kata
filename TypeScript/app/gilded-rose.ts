97c70a29bce8fea504b15a2537d6b6bab775dc65
karaaliidris <karaaliidris@gmail.com>
2022-07-26 15:58:25 +0300

    Before break, conditions merged.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -23,47 +23,38 @@ export class GildedRose {
   */
   updateQuality() {
     this.items.forEach (item => {
-      const isSulfuras = item.name == 'Sulfuras, Hand of Ragnaros'
-      const isAgedBrie = item.name == 'Aged Brie'
-      const isBackstage = item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      const isNormalItem = !isAgedBrie && !isBackstage && !isSulfuras
+      const isSulfuras = item.name == 'Sulfuras, Hand of Ragnaros';
+      const isAgedBrie = item.name == 'Aged Brie';
+      const isBackstage = item.name == 'Backstage passes to a TAFKAL80ETC concert';
+      const isConjuredItem = item.name.includes('Conjured');
+      const isNormalItem = !isAgedBrie && !isBackstage && !isSulfuras && !isConjuredItem;
 
       if (!isSulfuras) {
         item.sellIn --;
       }
 
-      if (isNormalItem && item.quality > 0) {
+      if (isNormalItem){
+        if (item.quality > 0)
+        {
+          item.quality --;
+          if (item.sellIn < 0){
             item.quality --;
-      } else {
-        if (item.quality < 50) {
+          }
+        }
+      }else if (isBackstage){
+        item.quality ++;
+        if(item.sellIn <= 10){
           item.quality ++;
-          if (isBackstage) {
-            if (item.sellIn <= 10 && item.quality < 50) { //changed
-                item.quality ++;
-            }
-            if (item.sellIn <= 5 && item.quality < 50) {
-                item.quality ++;
-            }
-          }
         }
+        if(item.sellIn <= 5){
+          item.quality ++;
+        }
+        if(item.sellIn < 0){
+          item.quality = 0;
+        }
+      }else if(isAgedBrie && item.quality < 50){
+        item.quality ++;
       }
-      
-      if (item.sellIn < 0 ) {
-          if (isNormalItem) {
-            if (item.quality > 0 ) {
-                item.quality --;
-            }
-          } else {
-            //item.quality = item.quality - item.quality
-            item.quality = 0;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality ++;
-          }
-        }
-
-      
     });
 
     return this.items;
